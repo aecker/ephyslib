ff1eb05c8f5917f417b101173ba0bc0181f6a146
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-08-30 20:24:56 +0200

    Changed schema for +ae

--- +ae/getSchema.m
+++ +ae/getSchema.m
@@ -2,6 +2,6 @@ function obj = getSchema
 persistent schemaObject
 if isempty(schemaObject)
     acq.getSchema();
-    schemaObject = dj.Schema(dj.conn, 'ae', 'aecker');
+    schemaObject = dj.Schema(dj.conn, 'ae', 'ae');
 end
 obj = schemaObject;
