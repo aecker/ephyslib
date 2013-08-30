8d06750d3fb7fe3f671199bb0aae91781ce45d5d
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-08-30 20:24:42 +0200

    Added depth info to TetrodeProperties

--- +ae/TetrodeProperties.m
+++ +ae/TetrodeProperties.m
@@ -7,6 +7,8 @@ electrode_num   : tinyint unsigned   # electrode number
 material        : ENUM("NiCh", "PtIr")  # tetrode material
 loc_x           : float     # x coordinate
 loc_y           : float     # y coordinate
+depth_to_brain = NULL : float     # adjustments until in brain (mu)
+depth_to_wm = NULL    : float     # adjustments until in white matter (mu)
 %}
 
 classdef TetrodeProperties < dj.Relvar
